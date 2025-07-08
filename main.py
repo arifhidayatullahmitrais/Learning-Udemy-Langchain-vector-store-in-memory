b4ca3253634fcedf444d8ed3b3c7f1b3cd3085d4
Arif Hidayatullah <arif.hidayatullah@mitrais.com>
2025-07-08 12:04:16 +0700

    save local vector store

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from pydantic import SecretStr
 
 load_dotenv()
 
@@ -9,5 +13,16 @@ if __name__ == '__main__':
     print(os.environ["OPENAI_API_KEY"])
     pdf_path = "/Users/arif_h667/Learning/Udemy/Langchain/vector-store-in-memory/example.pdf"
     loader = PyPDFLoader(file_path=pdf_path)
-    document = loader.load()
-    print(document)
+    documents = loader.load()
+    print(documents)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=25, separator="\n")
+    docs = text_splitter.split_documents(documents=documents)
+    print(docs)
+
+    embeddings = OpenAIEmbeddings(
+        api_key=SecretStr(os.environ["OPENAI_API_KEY"]),
+        model=os.environ["OPENAI_EMBEDDING_MODEL"],
+    )
+
+    vector_store = FAISS.from_documents(docs, embeddings)
+    vector_store.save_local("faiss_index_react")
